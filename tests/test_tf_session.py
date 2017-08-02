fb7c139af3d93aab0668787215920ddd73136ab3
jicius <bq_ji@yahoo.com>
2017-08-02 13:38:47 +0800

    fix: add tf test

--- tests/test_tf_session.py
+++ tests/test_tf_session.py
@@ -18,12 +18,12 @@
 
 import tensorflow as tf
 
-# tensorflow 的两个阶段: 定义计算图中的所有计算; 执行计算
-# tensorflow中所有的数据都是通过张量形式来表示
-# 张量的使用大致分为两类: 第一类用于对中间结果的引用; 第二类用于获取最终的计算结果
 
 a = tf.constant([1.0, 2.0], name="a")
 b = tf.constant([2.0, 3.0], name="b")
-result = a+b
-print a.graph is tf.get_default_graph()
-print result
+
+result = a + b
+
+with tf.Session() as sess:
+    sess.run(result)
+    print result
