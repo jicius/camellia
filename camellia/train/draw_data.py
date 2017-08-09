e30dbea2fe18a408904e835151e4efe0274581e4
jicius <bq_ji@yahoo.com>
2017-08-09 11:46:35 +0800

    add train

---
+++ camellia/train/draw_data.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#   -*- coding: utf-8 -*-
+#
+#   Copyright (C) 2017 Jicius
+# 
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+# 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import io
+
+import requests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = 'http://blog.topspeedsnail.com/wp-content/uploads/2016/12/铁路客运量.csv'
+ass_data = requests.get(url)
+
+print(ass_data)
+
